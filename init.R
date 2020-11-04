2ad20316dcd7d0b6a8f9cd974d50c4081c43b7de
Mathijs995 <mathijsdejong1995@gmail.com>
2020-11-04 19:10:08 +0100

    Deleted init.R

--- init.R
+++
@@ -1,7 +0,0 @@
-# Set working directory
-BASE.DIR = '/Users/mathijs/Google Drive/Tinbergen - MPhil/'
-WEEK = 'Week 2'
-setwd(paste0(BASE.DIR, '/Supervised Machine Learning/', WEEK, '/Assignment'))
-
-# Specify options
-options(scipen=999)
